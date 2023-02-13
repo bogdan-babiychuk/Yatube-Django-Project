346ac713f2b3890040684a91c86779f31f802873
Бабийчук Богдан <babiychuk.bogdan@mail.ru>
2023-02-13 22:22:49 +0300

    11

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,10 +1,14 @@
-
+import shutil
+import tempfile
+from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.shortcuts import get_object_or_404
-from django.test import Client, TestCase
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 from django import forms
 from posts.models import Group, Post, User
+
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
 class TestView(TestCase):
@@ -172,9 +176,13 @@ class PaginatorViewsTest(TestCase):
         self.assertEqual(len(response.context['page_obj']), 3)
 
 
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class ImageExsitsContext(TestCase):
-    def setUpClass(cls):
+    @classmethod
+    def setUpclass(cls):
+        super().setUpClass()
         cls.user = User.objects.create_user(username='wtf')
+        cls.authorized_client.force_login(cls.user)
         cls.small_gif = (
             b'\x47\x49\x46\x38\x39\x61\x02\x00'
             b'\x01\x00\x80\x00\x00\x00\x00\x00'
@@ -184,13 +192,17 @@ class ImageExsitsContext(TestCase):
             b'\x0A\x00\x3B'
         )
 
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
     def setUp(self):
         self.guest_client = Client()
         self.authorized_client = Client()
-        self.authorized_client.force_login(self.user)
 
     def test_urls_have_img(self):
-
         uploaded = SimpleUploadedFile(
             name='small.gif',
             content=self.small_gif,
@@ -201,19 +213,20 @@ class ImageExsitsContext(TestCase):
             slug='test-slug',
             description='test-описание группы'
         )
-
         self.post = Post.objects.create(
             author=self.user,
             text='text-текст',
             group=self.group,
             image=uploaded,
-        )
 
+
+        )
         urls = {
             reverse('posts:index'),
             reverse('posts:group_posts', kwargs={'slug': 'test-slug'}),
             reverse('posts:profile', kwargs={'username': 'wtf'}),
         }
+
         for page in urls:
             with self.subTest(post=page):
                 response = self.guest_client.get(page)
@@ -226,6 +239,8 @@ class ImageExsitsContext(TestCase):
             name='small.gif',
             content=self.small_gif,
             content_type='image/gif'
+
+
         )
 
         self.group = Group.objects.create(
@@ -244,7 +259,5 @@ class ImageExsitsContext(TestCase):
         response_for_detail = self.guest_client.get(
             reverse('posts:post_detail', kwargs={'post_id': self.post.pk})
         )
-
         img_for_detail = response_for_detail.context['post'].image.name
-
         self.assertIn(self.post.image.name, img_for_detail)
